a04441d8538330fabf10a770d7ee9755bbcb2243
zhangkaiao <zhangkaiao@youzidata.com>
2026-02-24 14:26:35 +0800

    新增

--- src/main/java/com/manual/Main.java
+++ src/main/java/com/manual/Main.java
@@ -3,5 +3,6 @@ package com.manual;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("This is a new branch");
     }
 }
