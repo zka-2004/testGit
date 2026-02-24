f90a3c720af3db2e03f667a4df34caf9ab17b31e
zhangkaiao <zhangkaiao@youzidata.com>
2026-02-24 14:38:45 +0800

    create new branch feature-02

--- src/main/java/com/manual/Main.java
+++ src/main/java/com/manual/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("This is a new branch");
+        System.out.println("This is a new branch feature-02...");
     }
 }
